832af9a30f0e85604afd12ab1e8769a76c484f8e
tomalmahadi <64693660+tomalmahadi@users.noreply.github.com>
2020-06-30 20:52:53 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set the input x as a matrix and then set solve value "z" as a NULL
 
-## Write a short comment describing this function
+## Changed every reference to "mean" to "Inverse"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        z = NULL
+        set <- function(y){
+                x <<- y
+                z <<- NULL
+  }
+        get  <- function()x
+        setInverse <- function(inverse) z <<-inverse
+        getInverse <- function() z
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)    
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        z <- x$getInverse()
+        if(is.null(z)){
+        message("getting cached data")
+        return(z)
+        }
+        mat <- x$get()
+        z <- solve(mat,...)
+        x$setInverse(z)
+        z
 }
